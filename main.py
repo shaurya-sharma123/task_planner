000d82fcf883b6c9308e043ecc54623256c48827
shaurya-sharma123 <shaurya.s.hp@gmail.com>
2026-06-02 23:36:28 +0530

    Initial commit: Completed functional FastAPI Task Planner with Gemini integration

---
+++ main.py
@@ -0,0 +1,57 @@
+import os 
+from typing import Annotated
+from fastapi import FastAPI, Body, HTTPException
+from google import genai
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI(title="AI Agent Task Planner")
+ai_client = genai.Client()
+
+class Task_Step(BaseModel):
+    step_id: int = Field(gt=0, description="The sequence number")
+    action: str = Field(min_length=5, max_length=200, description="The task the agent has to perform")
+    tools_required: str # The tools required for the search like "web_search", etc.
+
+class AI_Planner_Request(BaseModel):
+    user_goal: str = Field(min_length=10, description="The objective of the user")
+    temperature: float = Field(default=0.2, gt=0.0, le=1.0)
+    planned_steps: list[Task_Step]
+
+@app.post("/items/")
+async def run_agent_planner(
+    agent_request: Annotated[AI_Planner_Request, Body(embed=True)]
+):
+    if not agent_request.planned_steps:
+        raise HTTPException(status_code=400, detail="The Agent cannot an Empty Planner")
+    
+    execution_instruction = ""
+    
+    for step in agent_request.planned_steps:
+        execution_instruction += f"-Step {step.step_id} [{step.tools_required}]: {step.action}\n"
+    
+    prompt = f"""
+You are an elite AI agent executor. 
+You ultimate goal is: {agent_request.user_goal}
+Execute these sequential steps perfectly: {execution_instruction}
+Provide a comprehensive final report explaining these things perfectly and in a proper manner.
+"""
+    
+    try:
+        response = ai_client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = prompt,
+            config = {"temperature": agent_request.temperature}
+        )
+
+        return {
+            "status": "success",
+            "goal_processed": agent_request.user_goal,
+            "steps_executed_count": agent_request.planned_steps,
+            "agent_final_report": response.text
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Gemini Engine Error: {str(e)}")
+    
